4d2147e7beb1ee3d952419b21e58b20c4e47514d
bengrunfeld <benjamin.grunfeld@gmail.com>
2024-09-07 23:18:36 +0300

    init

---
+++ apps/web/components/HomePage/HomePage.tsx
@@ -0,0 +1,52 @@
+"use client";
+import CheckBox from "@repo/ui/CheckBox";
+import TextInput from "@repo/ui/TextInput";
+import { FieldValues } from "react-hook-form";
+import { useForm } from "react-hook-form";
+import styled from "styled-components";
+
+const StyledContainer = styled.div`
+  padding: 50px;
+  display: flex;
+  flex-direction: column;
+  gap: 50px;
+`;
+
+export default function HomePage() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const handleFormSubmit = (data: FieldValues) => {
+    console.log(data);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(handleFormSubmit)}>
+      <StyledContainer>
+        <TextInput
+          labelText="First Name"
+          legendText="Enter your first name"
+          name="first-name"
+          register={register}
+        />
+
+        <div>
+          <CheckBox
+            name="agree-to-terms"
+            register={register}
+            labelText="This is a checkbox"
+            errorMessage="This field is required"
+            error={errors?.["agree-to-terms"]}
+            required
+          />
+        </div>
+        <div>
+          <input type="submit" />
+        </div>
+      </StyledContainer>
+    </form>
+  );
+}
